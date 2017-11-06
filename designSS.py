75c6e173345a56010727e2ebcc48c4e14173882b
eerhan <eerhan@sfu.ca>
2017-11-06 11:03:13 -0800

    Changed designSS to be pythonic, fit pep8 standards, and be compatible with python3

--- designSS.py
+++ designSS.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python
-import sys, getopt, random
+import sys
+import random
+import argparse
 #  designSS.py
 #  SSDesign
 #
 #  Created by Birol on 2016-03-14.
+#  Modified by Emre Erhan
 #  Copyright (c) 2016 Birol. All rights reserved.
 
 # usage:
@@ -11,84 +14,54 @@ import sys, getopt, random
 # rules
 # m < n
 # l > n
-def usage():
-    scriptName=sys.argv[0]
-    print("SYNOPSIS")
-    print("\t%s [-n number_of_seeds] [-m allowed_number_of_misses] [-l seed_length]" %scriptName)
-    print("DESCRIPTION")
-    print("\tDesigns n spaced seeds of length l. Each bit location would have n-m seeds")
-    print("\trequiring a match. The set of seeds will be symetric to accommodate reverse")
-    print("\tcomplements.")
-    print("OPTIONS")
-    print("\t-n|--seeds")
-    print("\t\tnumber of spaced seeds to be designed (4)")
-    print("\t-m|--misses")
-    print("\t\tallowed number of missed hits (2). should be less than n")
-    print("\t-l|--seedlen")
-    print("\t\tseed length (100). should be more than n")
-    print("\t-e|--minlen")
-    print("\t\tminimum seed length (0) of at least one seed.")
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Designs n spaced seeds of length l. Each bit location would have n-m seeds\
+                    requiring a match. The set of seeds will be symmetric to accommodate reverse\
+                    complements")
+    parser.add_argument("-n", "--nseeds", type=int, default=4, help="Number of seeds (4)")
+    parser.add_argument("-m", "--misses", type=int, default=2,
+                        help="Number of allowed misses. Should be greater than nseeds (2)")
+    parser.add_argument("-l", "--seedlen", type=int, default=100, help="Seed length. Should be more than nseeds (100)")
+    parser.add_argument("-e", "--minlen", type=int, default=0, help="Minimum seed length (0)")
+    parser.add_argument("-d", "--debug", action='store_true')
+    args = parser.parse_args()
+    return args
 
 
 def main():
     # get run time parameters
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "hn:m:l:e:", ["help", "nseeds=", "misses=", "seedlen="])
-    except getopt.GetoptError as err:
-        # print help information and exit:
-        print str("option not recognized")
-        usage()
-        sys.exit(2)
-    nseeds = 4 # default number of seeds
-    misses = 2 # default number of allowed number of misses
-    seedlen = 100 # default seed length
-    minlen = 0
-    for opt, arg in opts:
-        if opt in ("-n", "--nseeds"):
-            nseeds = int(arg)
-        elif opt in ("-m", "--missees"):
-            misses = int(arg)
-        elif opt in ("-l", "--seedlen"):
-            seedlen = int(arg)
-        elif opt in ("-e", "--minlen"):
-            minlen = int(arg)
-        elif opt in ("-h", "--help"):
-            usage()
-            sys.exit()
-        else:
-            assert False, "unhandled option"
-    if (misses >= nseeds or seedlen <= nseeds):
-        # print help information and exit:
-        print str("### parameters violate assumptions!")
-        usage()
-        sys.exit(3)
-    
-    # initialize seeds
-    seed=[[1]*((seedlen+1)//2)+[0]*(seedlen//2) for i in range(nseeds)]
-    
-    currMinLen = -1
-    while minlen >= currMinLen:
-        seed=[[1]*((seedlen+1)//2)+[0]*(seedlen//2) for i in range(nseeds)]
-        seedList=range(0,nseeds)
+    args = parse_args()
+    nseeds = args.nseeds  # number of seeds
+    misses = args.misses  # number of allowed number of misses
+    seedlen = args.seedlen  # seed length
+    minlen = args.minlen
+    current_min_length = -1
+    while minlen >= current_min_length:
+        # initialize seeds
+        seed = [[1]*((seedlen+1)//2)+[0]*(seedlen//2) for _ in range(nseeds)]
+        seed_list = range(0, nseeds)
         # scan through the seed length/2
-        for i in range(0,((seedlen+1)//2)):
+        for i in range(0, ((seedlen+1)//2)):
             # select seeds to introduce don't care positions
-            dontcare=random.sample(seedList,nseeds-misses)
+            dont_care = random.sample(seed_list, nseeds-misses)
             # insert don't care positions
-            for j in dontcare:
-                seed[j][i]=0
+            for j in dont_care:
+                seed[j][i] = 0
         # make seed set symmetric
         for j in range(nseeds):
             for i in range(seedlen//2):
-                seed[nseeds-j-1][seedlen-i-1]=seed[j][i]
+                seed[nseeds-j-1][seedlen-i-1] = seed[j][i]
                 
         for j in range(nseeds):
             weight = 0
             for i in range(seedlen):
                 if seed[j][i] != 0:
                     weight += 1
-            if weight > currMinLen:
-                currMinLen = weight
+            if weight > current_min_length:
+                current_min_length = weight
         
     # output seeds
     for j in range(nseeds):
@@ -98,15 +71,16 @@ def main():
             if seed[j][i] != 0:
                 weight += 1
         sys.stdout.write("\t" + str(weight) + "\t" + str(seedlen-weight))
-        if weight > currMinLen:
-            currMinLen = weight
-        print
+        if weight > current_min_length:
+            current_min_length = weight
+        print()
         
     for j in range(nseeds):
         for i in range(seedlen):
             sys.stdout.write(str(seed[j][i]))
         sys.stdout.write(" ")
-    print
+    print()
+
 
 if __name__ == "__main__":
     main()
