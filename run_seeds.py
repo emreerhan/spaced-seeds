c1276faeb8a5561090cc7e4ad9ead2114706ad28
eerhan <eerhan@sfu.ca>
2017-11-17 14:58:59 -0800

    Test parameters set.

--- run_seeds.py
+++ run_seeds.py
@@ -1,3 +1,4 @@
+import argparse
 import designSS
 import pyfaidx
 import itertools
@@ -5,6 +6,18 @@ import math
 import numpy as np
 import pandas as pd
 from scipy.special import comb
+
+
+# def parse_args():
+#     parser = argparse.ArgumentParser(
+#         description="""Runs a number of spaced seeds against specified genomes
+#                     to determine if seed entropy affects specificity""")
+#     parser.add_argument("-i", "--input", type=str, help="Barcode whitelist path", required=True)
+#     parser.add_argument("-m", "--molecules", type=str, help="Molecules/amplicons fasta file", required=True)
+#     parser.add_argument("-o", "--output", type=str, help="Output file (default: stdout)")
+#     parser.add_argument("-s", "--random-seed", type=int, help="Define a seed (default: no seed)")
+#     args = parser.parse_args()
+#     return args
 
 
 def search_sequence(sequence, spaced_seed, kmer):
@@ -88,22 +101,36 @@ def get_random_seeds(k, w, num, random_seed=False):
     return seeds
 
 
+def get_random_kmers(k, num, random_seed=False):
+    bases = ['A', 'C', 'G', 'T']
+    if random_seed:
+        np.random.seed(random_seed)
+    kmers = []
+    for _ in range(num):
+        repeated_kmer = True
+        while repeated_kmer:
+            kmer = "".join(np.random.choice(bases, size=k, replace=True))
+            repeated_kmer = True if kmer in kmers else False
+        kmers.append(kmer)
+    return kmers
+
+
 def main():
     # seed = designSS.design_seed()
     bases = ['A', 'C', 'G', 'T']
     ecoli_k12 = pyfaidx.Fasta('e_coli.fa.gz')[0][0:]
     ecoli_BW25113 = pyfaidx.Fasta('e_ecoli_BW25113.fa')[0][0:]
     yeast = pyfaidx.Fasta('Pichia_sorbitophila.fa')[0][0:]
-    genomes = {'ecoli k12': ecoli_k12}
+    genomes = {'E. Coli k12': ecoli_k12, "E. Coli BW25113": ecoli_BW25113, "Pichia sorbitophila": yeast}
     # print(search_sequence('AAACAAAAGTAACG', '1000011', 'CGT'))
-    k = 6
-    w = 2
-    num_seeds = 1
-    kmers = ['AC', 'CT', 'GA']
+    k = 60
+    w = 20
+    num_seeds = 20
+
+    kmers = get_random_kmers(w, 20)
     seeds = get_random_seeds(k, w, num_seeds)
     calculate_entropy_vect = np.vectorize(calculate_entropy, excluded=['s_size'])
     entropies = calculate_entropy_vect(seeds, 3)
-    print(seeds)
     for kmer in kmers:
         data = pd.DataFrame(entropies, index=seeds, columns=['entropies'])
         for genome_name, genome in genomes.items():
