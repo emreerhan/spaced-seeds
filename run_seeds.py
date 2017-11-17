6c05b89dfcd7e86269e52f2546dc3d8624b17b26
eerhan <eerhan@sfu.ca>
2017-11-17 12:07:17 -0800

    Changed random seed generation to be more efficient. Sequence searching is no longer done via vectorized numpy function.

--- run_seeds.py
+++ run_seeds.py
@@ -1,4 +1,3 @@
-import decimal
 import designSS
 import pyfaidx
 import itertools
@@ -44,32 +43,55 @@ def calculate_entropy(seed, s_size):
         if count == 0:
             continue
         num_substrings = len(seed) - s_size + 1
-        entropy -= float(count)/float(num_substrings) * math.log(decimal.Decimal(float(count)/float(num_substrings)), 2)
+        entropy -= float(count)/float(num_substrings) * math.log(float(count)/float(num_substrings), 2)
     return entropy
 
 
-def main():
+def get_random_seeds(k, w, num, random_seed=False):
+    if random_seed:
+        np.random.seed(random_seed)
+    if num > comb(k, w):
+        raise ValueError("num cannot be greater than k choose w")
+    # Choose the indices to be 1
+    seeds = []
+    for _ in range(num):
+        repeated_seed = True
+        while repeated_seed:
+            seed = ""
+            indices = np.random.choice([i for i in range(k)], size=w, replace=False)
+            for i in range(k):
+                if i in indices:
+                    seed += "1"
+                else:
+                    seed += "0"
+            repeated_seed = True if seed in seeds else False
+        seeds.append(seed)
+    return seeds
 
+
+def main():
     # seed = designSS.design_seed()
     bases = ['A', 'C', 'G', 'T']
     ecoli_k12 = pyfaidx.Fasta('e_coli.fa.gz')[0][0:]
     ecoli_BW25113 = pyfaidx.Fasta('e_ecoli_BW25113.fa')[0][0:]
     yeast = pyfaidx.Fasta('Pichia_sorbitophila.fa')[0][0:]
     # print(search_sequence("AAACAAAAGTAACG", "1000011", "CGT"))
-    k = 10
-    w = 4
-    num_kmers = 10
-    kmers = []
-    seeds = np.array(["".join(seed) for seed in itertools.permutations("1"*w+"0"*(k-w), k)], dtype=str)
+    k = 6
+    w = 2
+    num_seeds = 1
+    kmer = "AC"
+    seeds = get_random_seeds(k, w, num_seeds)
     calculate_entropy_vect = np.vectorize(calculate_entropy, excluded=['s_size'])
     entropies = calculate_entropy_vect(seeds, 3)
-    print(seeds.shape)
-    print(entropies.shape)
-    pd.DataFrame(entropies, index=seeds, columns=["entropies"]).to_csv("test.tsv", sep='\t')
-
-    # for i in range(num_kmers):
-    #     kmers.append(np.random.choice(bases, w, replace=True))
-    #     calculate_entropy(seed, 3)
+    data = pd.DataFrame(entropies, index=seeds, columns=["entropies"])
+    # data.to_csv("test2.tsv", sep='\t')
+    hits = np.zeros(shape=num_seeds, dtype=bool)
+    for genome in [ecoli_k12]:  # , ecoli_BW25113, yeast]:
+        for i in range(len(seeds)):
+            seed = seeds[i]
+            hits[i] = search_sequence(genome, seed, kmer)
+    data.append(hits)
+    data.to_csv("test.tsv", sep='\t')
 
 
 if __name__ == "__main__":
